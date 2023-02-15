0d9ecf5b1f886406ba18fef8fd90c8669a11abbf
virginia-zuror <virginiazuor@gmail.com>
2023-02-15 09:11:53 +0100

    home fetch topartists

--- LastFM_Api/lastFM_Api/src/components/pages/Home.jsx
+++ LastFM_Api/lastFM_Api/src/components/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import './Home.css'
+
 
 const Home = () => {
   const APIKEY= '88d09abd9037b51fd61e9ccc60d66b1a';
@@ -12,15 +14,57 @@ const Home = () => {
   }
   useEffect(() => {
     getTopMusic();
-    console.log(topMusic);
+    
   }, [])
-  
+ 
 
 
   return (
-    <div>
-      {topMusic.map((artist)=><figure key={artist.name}>{artist.name}</figure>)}
-    </div>
+    <main>
+    <h2>Top 10 Artists in Spain</h2>
+    <div className='topMusic'>
+      <figure>      
+        <a href={topMusic[0]?.url}><h2>{topMusic[0]?.name}</h2></a>
+        <h4>Listeners: {topMusic[0]?.listeners}</h4>
+      </figure>
+      <figure>        
+        <a href={topMusic[1]?.url}><h2>{topMusic[1]?.name}</h2></a>
+        <h4>Listeners: {topMusic[1]?.listeners}</h4>
+      </figure>
+      <figure>    
+        <a href={topMusic[2]?.url}><h2>{topMusic[2]?.name}</h2></a>
+        <h4>Listeners: {topMusic[2]?.listeners}</h4>
+      </figure>
+      <figure>   
+        <a href={topMusic[3]?.url}><h2>{topMusic[3]?.name}</h2></a>
+        <h4>Listeners: {topMusic[3]?.listeners}</h4>
+      </figure>
+      <figure>        
+        <a href={topMusic[4]?.url}><h2>{topMusic[4]?.name}</h2></a>
+        <h4>Listeners: {topMusic[4]?.listeners}</h4>
+      </figure>
+      <figure>       
+        <a href={topMusic[5]?.url}><h2>{topMusic[5]?.name}</h2></a>
+        <h4>Listeners: {topMusic[5]?.listeners}</h4>
+      </figure>
+      <figure>        
+        <a href={topMusic[6]?.url}><h2>{topMusic[6]?.name}</h2></a>
+        <h4>Listeners: {topMusic[6]?.listeners}</h4>
+      </figure>
+      <figure>        
+        <a href={topMusic[7]?.url}><h2>{topMusic[7]?.name}</h2></a>
+        <h4>Listeners: {topMusic[7]?.listeners}</h4>
+      </figure>
+      <figure>
+        <a href={topMusic[8]?.url}><h2>{topMusic[8]?.name}</h2></a>
+        <h4>Listeners: {topMusic[8]?.listeners}</h4>
+      </figure>
+      <figure>
+        <a href={topMusic[9]?.url}><h2>{topMusic[9]?.name}</h2></a>  
+        <h4>Listeners: {topMusic[9]?.listeners}</h4>
+      </figure>
+   </div>
+   </main>
   )
 }
 
